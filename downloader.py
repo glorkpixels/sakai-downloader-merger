e2947e6f1531bb76bdde2c32a9eb9474237ca1f8
glorkpixels <44683546+glorkpixels@users.noreply.github.com>
2021-02-08 23:48:11 +0300

    exes

--- downloader.py
+++ downloader.py
@@ -2,34 +2,43 @@ import requests
 import urllib.request
 import moviepy.editor as mpe
 import os
+from time import sleep
 lines = []
-with open("link.txt") as file_in:
-    
-    for line in file_in:
-        lines.append(line)
-link = lines[0]
-link1 = lines[1]
-
-os.remove("link.txt")
-name = "video.mp4"
-name2 = "sound.mp4"
-
 try:
-    print("sound Downloading starts...\n")
-    urllib.request.urlretrieve(link1, name2)
-    resp = urllib.urlopen(link1)
-    respHtml = resp.read()
-    binfile = open(name2, "wb")
-    binfile.write(respHtml)
-    binfile.close()
-    print("sound Download completed..!!")
+        
+    with open("link.txt") as file_in:
+        
+        for line in file_in:
+            lines.append(line)
+    link = lines[0]
+    link1 = lines[1]
+
+    os.remove("link.txt")
+    name = "video.mp4"
+    name2 = "sound.mp4"
+
+    try:
+        print("sound Downloading starts...\n")
+        sleep(1)
+        urllib.request.urlretrieve(link1, name2)
+        resp = urllib.urlopen(link1)
+        respHtml = resp.read()
+        binfile = open(name2, "wb")
+        binfile.write(respHtml)
+        binfile.close()
+        print("sound Download completed..!!")
+    except Exception as e:
+            print(e)
+
+
+    try:
+        sleep(1)
+        print("video Downloading starts...\n")
+        urllib.request.urlretrieve(link, name)
+        print("video Download completed..!!")
+    except Exception as e:
+        print(e)
 except Exception as e:
         print(e)
 
 
-try:
-    print("video Downloading starts...\n")
-    urllib.request.urlretrieve(link, name)
-    print("video Download completed..!!")
-except Exception as e:
-    print(e)
